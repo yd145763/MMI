d4aebd743376642b3804f3568d826c22c2b0cec1
yd145763 <104641377+yd145763@users.noreply.github.com>
2025-01-03 23:54:59 +0800

    Update run_mmi.py

--- run_mmi.py
+++ run_mmi.py
@@ -47,8 +47,8 @@ path = 'C:\\Users\\Lim Yudian\\Documents\\mmi_gratings\\'
 files = os.listdir(path)
 
 # Filter the GDS files and extract their names without the extension
-gds_files = [filename[:-4] for filename in files if filename.endswith('.gds')]
-
+gds_files = [filename[:-4] for filename in files if filename.endswith('end.gds')]
+gds_files_base = [filename[:-9] for filename in files if filename.endswith('base.gds')]
 
 os.add_dll_directory("C:\\Program Files\\Lumerical\\v241\\api\\python\\lumapi.py") #the lumapi.py path in your pc, remember the double \\
 lumapi = imp.load_source("lumapi","C:\\Program Files\\Lumerical\\v241\\api\\python\\lumapi.py") #the lumapi.py path in your pc, remember the double \\
@@ -58,144 +58,196 @@ fdtd = lumapi.FDTD()
 import re
 
 for g in gds_files:
-    def extractfromstring(symbol, filename):
-        match = re.search(symbol+'(\\d+)_', filename)
-        if match:
-            number = match.group(1)
-            number = int(number)
-            in_micron = number/1000
-            for_fdtd = in_micron*1e-6
-            return for_fdtd
-    
-    width = extractfromstring('w', g)
-    width_list.append(width)
-    
-    height = extractfromstring('h', g)
-    height_list.append(height)
-    
-    taper_length = extractfromstring('tl', g)
-    taper_length_list.append(taper_length)
-    
-    taper_pitch = extractfromstring('tp', g)
-    taper_pitch_list.append(taper_pitch)
-    
-    gap = extractfromstring('g', g)
-    gap_list.append(gap)
-    
-    taper_width = extractfromstring('tw', g)
-    taper_width_list.append(taper_width)
-    
-    waveguide_width = extractfromstring('wgw', g)
-    waveguide_width_list.append(waveguide_width)
-    
-    pitch = extractfromstring('p', g)
-    pitch_list.append(pitch)
-    
-    
-    
-    fdtd.gdsimport("C:\\Users\\Lim Yudian\\Documents\\mmi_gratings\\"+g+".GDS", g, 1, "Si3N4 (Silicon Nitride) - Phillip", 0, 0.4e-6)
-    fdtd.set("name", g)
-    fdtd.set("z span", 0.4e-6)
-    fdtd.set("z", 0.0)
-    fdtd.set("x", 0.0)
-    fdtd.set("y", 0.0)
-    fdtd.set("z", 0.0)
-    
-    fdtd.addfdtd()
-    fdtd.set("dimension",1)
-    fdtd.set("x min",-1*((width/2)+(taper_length)+fdtd_margin))
-    fdtd.set("x max",((width/2)+(taper_length)+fdtd_margin))
-    fdtd.set("y min", -1*((height/2)+fdtd_margin))
-    fdtd.set("y max", ((height/2)+fdtd_margin))
-    fdtd.set("z", 0)
-    
-    fdtd.addport()
-    fdtd.set("name","port 1")
-    fdtd.set('x', -1*((width/2)+(taper_pitch/2)+(taper_length)))
-    fdtd.set('y min', (gap/2) + (taper_width/2) - (waveguide_width/2))
-    fdtd.set('y max', (gap/2) + (taper_width/2) + (waveguide_width/2))
-    fdtd.set('injection axis', 'x-axis')
-    fdtd.set('direction', 'Forward')
-    
-    fdtd.addport()
-    fdtd.set("name","port 2")
-    fdtd.set('x', ((width/2)+(taper_pitch/2)+(taper_length)))
-    fdtd.set('y min', (gap/2) + (taper_width/2) - (waveguide_width/2))
-    fdtd.set('y max', (gap/2) + (taper_width/2) + (waveguide_width/2))
-    fdtd.set('injection axis', 'x-axis')
-    fdtd.set('direction', 'Backward')
-    
-    fdtd.addport()
-    fdtd.set("name","port 3")
-    fdtd.set('x', -1*((width/2)+(taper_pitch/2)+(taper_length)))
-    fdtd.set('y min', (-gap/2) + (-taper_width/2) + (-waveguide_width/2))
-    fdtd.set('y max', (-gap/2) + (-taper_width/2) - (-waveguide_width/2))
-    fdtd.set('injection axis', 'x-axis')
-    fdtd.set('direction', 'Forward')
-    
-    fdtd.addport()
-    fdtd.set("name","port 4")
-    fdtd.set('x', ((width/2)+(taper_pitch/2)+(taper_length)))
-    fdtd.set('y min', (-gap/2) + (-taper_width/2) + (-waveguide_width/2))
-    fdtd.set('y max', (-gap/2) + (-taper_width/2) - (-waveguide_width/2))
-    fdtd.set('injection axis', 'x-axis')
-    fdtd.set('direction', 'Backward')
-    
-    fdtd.setglobalsource("wavelength start",1500e-9)
-    fdtd.setglobalsource("wavelength stop",1600e-9)
-    fdtd.setglobalmonitor("frequency points",11)
-    
-    fdtd.save('C:\\Users\\Lim Yudian\\Documents\\mmi_gratings\\script_run.fsp')
-    print("simulating......")
-    fdtd.run()
-    print("simulation done!")
-    
-    def getE(port, port_list):
-        E = fdtd.getresult("FDTD::ports::"+port,"E")
+    if g in gds_files_base:
+        def extractfromstring(symbol, filename):
+            match = re.search(symbol+'(\\d+)_', filename)
+            if match:
+                number = match.group(1)
+                number = int(number)
+                in_micron = number/1000
+                for_fdtd = in_micron*1e-6
+                return for_fdtd
+        
+        width = extractfromstring('w', g)
+        width_list.append(width)
+        
+        height = extractfromstring('h', g)
+        height_list.append(height)
+        
+        taper_length = extractfromstring('tl', g)
+        taper_length_list.append(taper_length)
+        
+        taper_pitch = extractfromstring('tp', g)
+        taper_pitch_list.append(taper_pitch)
+        
+        gap = extractfromstring('g', g)
+        gap_list.append(gap)
+        
+        taper_width = extractfromstring('tw', g)
+        taper_width_list.append(taper_width)
+        
+        waveguide_width = extractfromstring('wgw', g)
+        waveguide_width_list.append(waveguide_width)
+        
+        pitch = extractfromstring('p', g)
+        pitch_list.append(pitch)
+        
+        fdtd.gdsimport("C:\\Users\\Lim Yudian\\Documents\\mmi_gratings\\"+g+"_base.GDS", g+"_base", 1, "Si (Silicon) - Palik", -0.08e-6, 0) #
+        fdtd.set("name", g+"_base")
+        fdtd.set("x", 0.0)
+        fdtd.set("y", 0.0)
+        fdtd.set("z", -0.11e-6)
+        
+        fdtd.gdsimport("C:\\Users\\Lim Yudian\\Documents\\mmi_gratings\\"+g+".GDS", g, 1, "Si (Silicon) - Palik", -0.11e-6, 0.11e-6) #
+        fdtd.set("name", g)
+        fdtd.set("x", 0.0)
+        fdtd.set("y", 0.0)
+        fdtd.set("z", 0.0)
+        
+
+        
+        fdtd.addfdtd()
+        fdtd.set("dimension",1)
+        fdtd.set("x min",-1*((width/2)+(taper_length)+fdtd_margin))
+        fdtd.set("x max",((width/2)+(taper_length)+fdtd_margin))
+        fdtd.set("y min", -1*((height/2)+fdtd_margin))
+        fdtd.set("y max", ((height/2)+fdtd_margin))
+        fdtd.set("z", 0)
+        fdtd.set("background material", "SiO2 (Glass) - Palik")
+        
+        fdtd.addpower()
+        fdtd.set("name","E")
+        fdtd.set("monitor type",7)
+        fdtd.set("x min",-1*((width/2)+(taper_length)))
+        fdtd.set("x max",((width/2)+(taper_length)))
+        fdtd.set("y min", -1*((height/2)))
+        fdtd.set("y max", ((height/2)))
+        fdtd.set("z", 0)
+        
+        fdtd.addport()
+        fdtd.set("name","port 1")
+        fdtd.set('x', -1*((width/2)+(taper_pitch/2)+(taper_length)))
+        fdtd.set('y min', (gap/2) + (taper_width/2) - (waveguide_width/2))
+        fdtd.set('y max', (gap/2) + (taper_width/2) + (waveguide_width/2))
+        fdtd.set('injection axis', 'x-axis')
+        fdtd.set('direction', 'Forward')
+        
+        fdtd.addport()
+        fdtd.set("name","port 2")
+        fdtd.set('x', ((width/2)+(taper_pitch/2)+(taper_length)))
+        fdtd.set('y min', (gap/2) + (taper_width/2) - (waveguide_width/2))
+        fdtd.set('y max', (gap/2) + (taper_width/2) + (waveguide_width/2))
+        fdtd.set('injection axis', 'x-axis')
+        fdtd.set('direction', 'Backward')
+        
+        fdtd.addport()
+        fdtd.set("name","port 3")
+        fdtd.set('x', -1*((width/2)+(taper_pitch/2)+(taper_length)))
+        fdtd.set('y min', (-gap/2) + (-taper_width/2) + (-waveguide_width/2))
+        fdtd.set('y max', (-gap/2) + (-taper_width/2) - (-waveguide_width/2))
+        fdtd.set('injection axis', 'x-axis')
+        fdtd.set('direction', 'Forward')
+        
+        fdtd.addport()
+        fdtd.set("name","port 4")
+        fdtd.set('x', ((width/2)+(taper_pitch/2)+(taper_length)))
+        fdtd.set('y min', (-gap/2) + (-taper_width/2) + (-waveguide_width/2))
+        fdtd.set('y max', (-gap/2) + (-taper_width/2) - (-waveguide_width/2))
+        fdtd.set('injection axis', 'x-axis')
+        fdtd.set('direction', 'Backward')
+        
+        fdtd.setglobalsource("wavelength start",1500e-9)
+        fdtd.setglobalsource("wavelength stop",1600e-9)
+        fdtd.setglobalmonitor("frequency points",11)
+        
+        fdtd.save('C:\\Users\\Lim Yudian\\Documents\\mmi_gratings\\script_run.fsp')
+        print("simulating......")
+        fdtd.run()
+        print("simulation done!")
+        
+        def getE(port, port_list):
+            E = fdtd.getresult("FDTD::ports::"+port,"E")
+            x1 = E["x"]
+            x1 = x1[:,0]
+            x1 = [i*1000000 for i in x1]
+            y1 = E["y"]
+            y1 = y1[:,0]
+            E1 = E["E"]
+            Ex1 = E1[:,:,:,0,0]
+            Ey1 = E1[:,:,:,0,1]
+            Ez1 = E1[:,:,:,0,2]
+            Emag1 = np.sqrt(np.abs(Ex1)**2 + np.abs(Ey1)**2 + np.abs(Ez1)**2)
+            Emag1 = Emag1[:,:,0]
+            Emag1 = Emag1[0,:]
+            port_list.append(Emag1)
+            plt.plot(y1, Emag1)
+            plt.title(g+port+" E")
+            plt.show()
+            plt.close()
+        
+        getE('port 1', port1_list)
+        getE('port 2', port2_list)
+        getE('port 3', port3_list)
+        getE('port 4', port4_list)
+        
+        def getT(port, port_list):
+            T = fdtd.getresult("FDTD::ports::"+port,"T")
+            wavelength = T['lambda']
+            T_abs = T['T']
+            port_list.append(T_abs)
+            plt.plot(wavelength, T_abs)
+            plt.title(g+port+" T")
+            plt.show()
+            plt.close()
+        
+        getT('port 1', port1T_list)
+        getT('port 2', port2T_list)
+        getT('port 3', port3T_list)
+        getT('port 4', port4T_list)
+        
+        E = fdtd.getresult("E","E")
+        E2 = E["E"]
+        Ex1 = E2[:,:,:,0,0]
+        Ey1 = E2[:,:,:,0,1]
+        Ez1 = E2[:,:,:,0,2]
+        Emag1 = np.sqrt(np.abs(Ex1)**2 + np.abs(Ey1)**2 + np.abs(Ez1)**2)
+        Emag1 = Emag1[:,:,0]
         x1 = E["x"]
         x1 = x1[:,0]
         x1 = [i*1000000 for i in x1]
         y1 = E["y"]
         y1 = y1[:,0]
-        E1 = E["E"]
-        Ex1 = E1[:,:,:,0,0]
-        Ey1 = E1[:,:,:,0,1]
-        Ez1 = E1[:,:,:,0,2]
-        Emag1 = np.sqrt(np.abs(Ex1)**2 + np.abs(Ey1)**2 + np.abs(Ez1)**2)
-        Emag1 = Emag1[:,:,0]
-        Emag1 = Emag1[0,:]
-        port_list.append(Emag1)
-        plt.plot(y1, Emag1)
-        plt.title(g+port+" E")
+        y1 = [j*1000000 for j in y1]
+        
+        Emag1 = Emag1.transpose()
+        Emag1_df = pd.DataFrame(Emag1)
+
+        fig,ax=plt.subplots(1,1)
+        cp=ax.contourf(x1,y1,Emag1, 200, zdir='z', offset=-100, cmap='jet')
+        clb=fig.colorbar(cp)
+        clb.ax.set_title('Electric Field (eV)', fontweight="bold")
+        for l in clb.ax.yaxis.get_ticklabels():
+            l.set_weight("bold")
+            l.set_fontsize(12)
+        ax.set_xlabel('x-position (µm)', fontsize=13, fontweight="bold", labelpad=1)
+        ax.set_ylabel('y-position (µm)', fontsize=13, fontweight="bold", labelpad=1)
+        ax.xaxis.label.set_fontsize(13)
+        ax.xaxis.label.set_weight("bold")
+        ax.yaxis.label.set_fontsize(13)
+        ax.yaxis.label.set_weight("bold")
+        ax.tick_params(axis='both', which='major', labelsize=13)
+        ax.set_yticklabels(ax.get_yticks(), weight='bold')
+        ax.set_xticklabels(ax.get_xticks(), weight='bold')
+        ax.xaxis.set_major_formatter(StrMethodFormatter('{x:,.1f}'))
+        ax.yaxis.set_major_formatter(StrMethodFormatter('{x:,.1f}'))
+        plt.title(g)
         plt.show()
         plt.close()
-    
-    getE('port 1', port1_list)
-    getE('port 2', port2_list)
-    getE('port 3', port3_list)
-    getE('port 4', port4_list)
-    
-    def getT(port, port_list):
-        T = fdtd.getresult("FDTD::ports::"+port,"T")
-        wavelength = T['lambda']
-        T_abs = T['T']
-        port_list.append(T_abs)
-        plt.plot(wavelength, T_abs)
-        plt.title(g+port+" T")
-        plt.show()
-        plt.close()
-    
-    getT('port 1', port1T_list)
-    getT('port 2', port2T_list)
-    getT('port 3', port3T_list)
-    getT('port 4', port4T_list)
-    
-    
-    fdtd.switchtolayout()
-    fdtd.deleteall()
-    fdtd.closeall()
-    
-    print("loading new gds......")
+        
+        fdtd.switchtolayout()
+        fdtd.deleteall()
+        fdtd.closeall()
+        
 
 df_results = pd.DataFrame()
 df_results['port1_list'] = port1_list
